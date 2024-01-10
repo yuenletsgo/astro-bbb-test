6a264518ff4b39e97ac55a2f58bd949256e827ab
yuenletsgo <hello@yuenw.design>
2024-01-10 21:41:27 +0800

    greeting btn style updated

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -9,9 +9,13 @@ export default function Greeting({messages}) {
   return (
     <div className='pl-2'>
       <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>
+      <div className='relative bg-slate-900 dark:bg-neutral-50 text-neutral-50 dark:text-slate-900 w-fit mt-6 px-4 py-2 rounded select-none cursor-pointer hover:opacity-90' onClick={() => setGreeting(randomMessage())}>
         New Greeting
-      </button>
+        {/* <span class="absolute -right-1 -top-1 flex h-3 w-3">
+          <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-rose-200 opacity-75"></span>
+          <span class="relative inline-flex rounded-full h-3 w-3 bg-rose-400"></span>
+        </span> */}
+      </div>
     </div>
   );
 }
