0780c303513f886708998e0208a4d3db4b553870
yuenletsgo <hello@yuenw.design>
2024-02-16 20:27:57 +0800

    config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  image: {
+    domains: ["astro.build"],
+  }
 });
