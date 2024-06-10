f4ee16923b6ada57e758bb39cb415fad3952df3b
yuenletsgo <hello@yuenw.design>
2024-06-10 12:06:25 +0800

    Add purge option

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,8 +4,8 @@ const plugin = require('tailwindcss/plugin');
 export default {
 	darkMode: 'class',
 	corePlugins: {
-    preflight: false,
-  },
+		preflight: false,
+	},
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
@@ -14,17 +14,17 @@ export default {
 			},
 		},
 	},
+	purge: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'], // Add purge option
 	plugins: [
 		plugin(function({ addBase, theme }) {
-      addBase({
-        'h1': { fontSize: 'clamp(1.5rem,3vw,4rem)', lineHeight: '95%', fontWeight: '500', },
-				// 'h1': { fontSize: 'clamp(2.5rem, -1.5rem + 8vw, 4rem)' },
-        'h2': { fontSize: 'clamp(1rem,1.5vw,3rem)', lineHeight: '95%', fontWeight: '300', letterSpacing: '0.035rem' },
-        'h3': { fontSize: 'clamp(1rem,1.5vw,3rem)', lineHeight: '95%', fontWeight: '300', letterSpacing: '0.035rem' },
+			addBase({
+				'h1': { fontSize: 'clamp(1.5rem,3vw,4rem)', lineHeight: '95%', fontWeight: '500', },
+				'h2': { fontSize: 'clamp(1rem,1.5vw,3rem)', lineHeight: '95%', fontWeight: '300', letterSpacing: '0.035rem' },
+				'h3': { fontSize: 'clamp(1rem,1.5vw,3rem)', lineHeight: '95%', fontWeight: '300', letterSpacing: '0.035rem' },
 				'h4': { fontSize: 'clamp(.8rem,1.25vw,1.625rem)', lineHeight: '0' },
 				'h5': { fontSize: 'clamp(.825rem,1.1vw,1.07rem)', lineHeight: '135%' },
 				'p': { fontSize: '1rem' },
-      })
-    })
+			})
+		})
 	],
 }
