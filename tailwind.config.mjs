162ab67b15374257e52a9e9ff13fd842e5ced199
yuenletsgo <hello@yuenw.design>
2024-06-10 15:10:56 +0800

    replace google font with Fontsource to improve efficacy

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,11 +12,17 @@ export default {
 			fontSize: {
 				'clamp': 'clamp(2rem,3vw,10vw)',
 			},
+			fontFamily: {
+				// sans: ['Inter', ...defaultTheme.fontFamily.sans],
+				// karla: ['Karla', ...defaultTheme.fontFamily.karla],
+				// mer: ['Merriweather', ...defaultTheme.fontFamily.mer],
+			},
 		},
 	},
 	plugins: [
 		plugin(function({ addBase, theme }) {
 			addBase({
+				// 'html': { fontFamily: theme('fontFamily.karla') },
 				'h1': { fontSize: 'clamp(1.5rem,3vw,4rem)', lineHeight: '95%', fontWeight: '500', },
 				'h2': { fontSize: 'clamp(1rem,1.5vw,3rem)', lineHeight: '95%', fontWeight: '300', letterSpacing: '0.035rem' },
 				'h3': { fontSize: 'clamp(1rem,1.5vw,3rem)', lineHeight: '95%', fontWeight: '300', letterSpacing: '0.035rem' },
